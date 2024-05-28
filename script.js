9ea66b929e214d474dc7ccc555b140653304539c
Md. Manuar Zaman <munna9757@gmail.com>
2024-05-28 17:03:46 +0600

    fixed some css

--- script.js
+++ script.js
@@ -4,13 +4,15 @@ const _back_to_top = document.querySelector('._back_to_top');
 
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
-if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    _back_to_top.style.bottom = "6vh";
-} else {
-    _back_to_top.style.bottom = "-30px";
-}
+    let top = 100; // pixel value from top
+    if (document.body.scrollTop > top || document.documentElement.scrollTop > top) {
+        _back_to_top.style.bottom = "6vh";
+    } else {
+        _back_to_top.style.bottom = "-30px";
+    }
 
 }
+
 _back_to_top.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
